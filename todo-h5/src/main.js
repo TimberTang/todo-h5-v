268afe7bd51860e0fa3b28d2c912cf2f81c25cc4
TimberTang <ming.tang@ndmicro.com>
2019-09-27 15:46:46 +0800

    init router

--- todo-h5/src/main.js
+++ todo-h5/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+import store from './store/index'
 
 Vue.config.productionTip = false
 
 new Vue({
+  Router,
+  store,
   render: h => h(App),
 }).$mount('#app')
