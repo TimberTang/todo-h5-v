505fb92d324803809f8386c5af5516683fcdea88
TimberTang <ming.tang@ndmicro.com>
2019-09-27 17:01:21 +0800

    init seconde

---
+++ todo-h5/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <transition>
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<style lang='less'>
+  #app {
+    -webkit-font-smoothing: antialiased; // 抗锯齿渲染
+    -moz-osx-font-smoothing: grayscale;
+    max-width: 720px;
+    margin: 0 auto;
+    background-color: white;
+    height: 100%;
+    color: #333;
+    position: relative;
+
+    html, body {
+      padding: 0;
+      margin: 0;
+      height: 100%;
+      -webkit-overflow-scrolling: touch;
+    }
+
+    /** {*/
+    /*    font-family: 'PingFangSC-Light', 'Avenir', Helvetica, 'Microsoft Yahei', Arial, sans-serif;*/
+    /*}*/
+    button {
+        border: none;
+        background-color: transparent;
+    }
+
+}
+</style>
---
+++ todo-h5/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
+<style scoped>
+  .home {
+    width: 100%;
+    height: 50px;
+    background-color: red;
+  }
+</style>
